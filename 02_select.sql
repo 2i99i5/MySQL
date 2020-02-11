ff361de5c6f0b5fb38f8038573973edf0d3037ec
2i99i5 <zakirovra@gmail.com>
2020-02-11 22:37:55 +0400

    6. характерные запросы.

--- 02_select.sql
+++ 02_select.sql
@@ -19,3 +19,29 @@ JOIN employee e ON e.id=bi.employee_id
 WHERE bi.office_id = @of_id AND bi.status_id not in (4, 5, 6)
 ORDER BY e.lastname
 ;
+
+-- список сотрудников филиала, номер филиала записывается в переменную @branchid
+SET @branchid = 1;
+SELECT
+	CONCAT(e.lastname, ' ', e.firstname, ' ', e.middlename) AS 'Cотрудник',
+	p.name AS 'Должность',
+	d.short_name AS 'Отдел',
+	e.phone AS 'Телефон',
+	e.email AS 'E-mail'
+FROM employee e
+JOIN `position`p ON p.id=e.pos_id
+JOIN division d ON d.id=e.division_id
+WHERE e.status = 'active' and e.branch_id = @branchid
+ORDER BY e.division_id
+;
+
+-- количество сотрудников в отделах филиала, номер филиала записывается в переменную @branchid
+SET @branchid = 1;
+SELECT
+	d.name,
+	COUNT(*)
+FROM employee e
+JOIN division d ON d.id=e.division_id
+WHERE e.status = 'active' and e.branch_id = @branchid
+group by e.division_id
+;
